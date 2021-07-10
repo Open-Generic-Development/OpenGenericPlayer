f0673338c3b329217f63e7e6b292faa1c841157d
Christian B <37768199+Bloeckchengrafik@users.noreply.github.com>
2021-07-10 18:30:57 +0200

    Add Song Controls

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 const {app, BrowserWindow} = require('electron');
 const {print} = require('./utils.js');
 
-function createWindow(){
+function createWindow() {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
     })
 
     win.setIcon("img/ogp-chan.png")
     win.loadFile("index.html").then()
 }
 
-print("Using OS: " + process.platform)
+// Enable live reload for all the files inside our project directory
+require('electron-reload')(
+    __dirname,
+    {
+        electron: require(`${__dirname}/node_modules/electron`)
+    }
+);
 
 app.whenReady().then(() => createWindow())
